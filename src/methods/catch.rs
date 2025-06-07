6f9e2659172f001c252db41ba152525f96354df0
Prokop <prokop.schield@protonmail.com>
2025-06-07 14:50:49 +0200

    refactor: use transformer in .catch()

--- src/methods/catch.rs
+++ src/methods/catch.rs
@@ -1,21 +1,20 @@
-use crate::{Promise, PromiseRejection};
+use crate::{Promise, PromiseRejection, Transformer};
 
 impl<T, E> Promise<T, E>
 where
     T: Send + Unpin + Sync + 'static,
     E: Send + Unpin + Sync + 'static,
 {
-    pub fn catch<TO, EO, CB>(self, callback: CB) -> Promise<TO, EO>
+    pub fn catch<TO, EO>(self, transformer: Transformer<E, TO, EO>) -> Promise<TO, EO>
     where
-        TO: From<T> + Unpin,
-        EO: Unpin,
-        CB: Send + FnOnce(E) -> Result<TO, EO> + Sync + 'static,
+        TO: From<T> + Unpin + 'static,
+        EO: Unpin + 'static,
     {
         let future = async move {
             match self.await {
                 Ok(value) => Ok(value.into()),
                 Err(err) => match err {
-                    PromiseRejection::Err(err) => match callback(err) {
+                    PromiseRejection::Err(err) => match (transformer.transform)(err).await {
                         Ok(value) => Ok(value),
                         Err(err) => Err(PromiseRejection::<EO>::Err(err)),
                     },
