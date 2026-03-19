1a45f6adff7fa9acf840b0dc0f69ba87a6f15953
Prokop Schield <prokop.schield@protonmail.com>
2026-03-19 21:54:56 +0100

    refactor: Promise::ready

--- src/methods/ready.rs
+++ src/methods/ready.rs
@@ -1,34 +1,17 @@
-use std::{
-    future::Future,
-    task::{Context, Poll},
-};
+use std::task::Context;
 
 use crate::{Promise, PromiseRejection};
 
 impl<T, E> Promise<T, E>
 where
-    T: Send + Unpin + 'static,
-    E: PromiseRejection + 'static,
+    T: Unpin,
+    E: PromiseRejection,
 {
     /// Polls the promise's inner future if pending.
     /// Returns `true` if the promise is now resolved or rejected.
     pub fn ready(&mut self, cx: &mut Context<'_>) -> bool {
-        if let Self::Pending(fut) = self {
-            // Just use fut.as_mut(), which is Pin<&mut ...>
-            match Future::poll(fut.as_mut(), cx) {
-                Poll::Pending => false,
-                Poll::Ready(Ok(value)) => {
-                    *self = Self::Resolved(value);
-                    true
-                }
-                Poll::Ready(Err(err)) => {
-                    *self = Self::Rejected(err);
-                    true
-                }
-            }
-        } else {
-            // Already resolved, rejected, or consumed
-            true
-        }
+        self.poll(cx);
+
+        self.is_ready()
     }
 }
