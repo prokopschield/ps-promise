37d8da961d99f9afc8d0893fda89c46d4573ee9b
Prokop Schield <prokop.schield@protonmail.com>
2026-06-07 12:44:26 +0200

    refactor: use Promise::eager_or_lazy in Promise::wrap

--- src/methods/wrap.rs
+++ src/methods/wrap.rs
@@ -4,10 +4,10 @@ use crate::{Promise, WrappedPromiseRejection};
 
 impl<T, E> Promise<T, WrappedPromiseRejection<E>>
 where
-    T: Unpin,
+    T: Send + Unpin + 'static,
     E: Send + Unpin + 'static,
 {
     pub fn wrap(future: impl Future<Output = Result<T, E>> + Send + 'static) -> Self {
-        Self::new(async move { Ok(future.await?) })
+        Self::eager_or_lazy(async move { Ok(future.await?) })
     }
 }
